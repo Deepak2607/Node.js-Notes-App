b333865ea56dff1c59d3895f52c70ec9655eea33
Deepak Kumrawat <deepakkumrawat8@gmail.com>
2018-08-11 16:58:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,20 +4,6 @@ const notes= require('./notes');
 const _= require('lodash');
 const yargs= require('yargs');
 
-//var userInfo= os.userInfo();
-//fs.appendFileSync('greetings.txt',`Hello ${userInfo.username} You are ${notes.age}` );
-
-//const filteredArray= _.uniq([1,1,1,2,3,4,2,2,4,3,2,1]);
-//console.log(_.isString(true));
-//console.log(_.isString('deepu'));
-//console.log(filteredArray);
-//
-//console.log(userInfo);
-//console.log(notes.add(1, 2));
-
-//console.log('process: ',process.argv);
-//const command=process.argv[2];
-
 const argv= yargs.argv;
 const command=argv._[0];
 console.log('command: ',command);
